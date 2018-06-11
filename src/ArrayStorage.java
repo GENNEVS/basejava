944a36887407d29dbe229596e4c29e0d5cb8c211
Gennadiy Evseev <evseevg@gmail.com>
2018-06-11 17:52:40 +0300

    Methods size, clear, save, getAll was implemented in ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,13 +1,23 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int tail = -1;
 
     void clear() {
+        Arrays.fill(this.storage, 0, this.size(), null);
+        this.tail = -1;
     }
 
     void save(Resume r) {
+        try {
+            this.storage[++this.tail] = r;
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 
     Resume get(String uuid) {
@@ -21,10 +31,16 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] list = null;
+        try {
+            list = Arrays.copyOfRange(this.storage, 0, this.size());
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return  list;
     }
 
     int size() {
-        return 0;
+        return this.tail + 1;
     }
 }
