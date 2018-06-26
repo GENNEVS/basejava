54e504eed760589cd84669e5d5ac7949f59799f4
Gennadiy Evseev <evseevg@gmail.com>
2018-06-26 23:42:07 +0300

    HW3: AbstractArrayStorage, ArrayStorage, and SortedArrayStorage were refactored.

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -11,6 +11,40 @@ public abstract class AbstractArrayStorage implements Storage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) >= 0) {
+            System.out.println("Resume is in the storage.");
+        } else if (size >= STORAGE_LIMIT){
+            System.out.println("Resume can't be saved: storage is full!");
+        } else {
+            insertResume(r);
+        }
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index != -1) {
+            storage[index] = r;
+        } else {
+            System.out.println("Resume isn't in the storage.");
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index != -1) {
+            System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("Resume isn't in the storage.");
+        }
+    }
 
     public Resume get(String uuid) {
         int index = getIndex(uuid);
@@ -23,10 +57,15 @@ public abstract class AbstractArrayStorage implements Storage {
         return null;
     }
 
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
 
     public int size() {
         return size;
     }
 
     protected abstract int getIndex(String uuid);
+
+    protected abstract void insertResume(Resume r);
 }
--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -8,54 +8,7 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-      public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void save(Resume r) {
-        if (size >= STORAGE_LIMIT) {
-            System.out.println("Resume " + r.getUuid() + " can't be saved: storage is full!");
-        } else if (getIndex(r.getUuid()) != -1){
-            System.out.println("Resume " + r.getUuid() + " is in the storage.");
-        } else {
-            storage[size++] = r;
-        }
-
-    }
-
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index != -1) {
-            storage[index] = r;
-        } else {
-            System.out.println("Resume  " + r.getUuid() + " isn't in the storage.");
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index != -1) {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        } else {
-            System.out.println("Resume " + uuid + " isn't in the storage.");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
-    /**
-     *
-     * @param uuid
-     * @return index of the element with uuid that equals to param
-     */
+    @Override
     protected int getIndex(String uuid) {
         for(int i = 0; i < size; i++ ) {
             if (uuid.equals(storage[i].getUuid())) {
@@ -63,5 +16,10 @@ public class ArrayStorage extends AbstractArrayStorage {
             }
         }
         return -1;
+    }
+
+    @Override
+    protected void insertResume(Resume r) {
+        storage[size++] = r;
     }
 }
